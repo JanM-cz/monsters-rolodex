97014cadcbad280d92f9b9310831cba2b08259cc
unknown <milhi@milhi.cz>
2020-01-07 22:04:08 +0100

    Updates

---
+++ precache-manifest.ef6af3f98abcd413716d85983a8198be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8da6bfc44426a4cbaeb2cc4698d1cd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f4d73992775150cd8604",
+    "url": "/monsters-rolodex/static/css/main.262edeef.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4d73992775150cd8604",
+    "url": "/monsters-rolodex/static/js/main.22d11f22.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
